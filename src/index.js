663cd198a250e7a413929a0c96df2c5ec89b012a
Leonardo Gatica <lgatica@protonmail.com>
2017-11-20 21:37:20 -0300

    style(standard): migrate to standard

--- src/index.js
+++ src/index.js
@@ -1,79 +1,82 @@
-'use strict';
+'use strict'
 
-const crc = require('crc');
-const pad = require('pad');
+const crc = require('crc')
+const pad = require('pad')
 
 const patterns = {
   mvt380: /^\$\$([\x41-\x7A])(\d{1,3}),(\d{15}),([0-9A-F]{3}),(\d{1,3}),([-]?\d+\.\d+),([-]?\d+\.\d+),(\d{12}),([AV]),(\d{1,3}),(\d{1,2}),(\d+(\.\d+)?),(\d+(\.\d+)?),(\d+(\.\d+)?),(\d+(\.\d+)?),(\d+(\.\d+)?),(\d+),(\d{3})\|(\d{1,3})\|([0-9A-F]{4})\|([0-9A-F]{4}),([0-9A-F]{4}),([0-9A-F]{1,4})?\|([0-9A-F]{1,4})?\|([0-9A-F]{1,4})?\|([0-9A-F]{1,4})\|([0-9A-F]{1,4}),([0-9A-F]{8})?,?([0-9A-F]+)?,?(\d{1,2})?,?([0-9A-F]{4})?,?([0-9A-F]{6})?\|?([0-9A-F]{6})?\|?([0-9A-F]{6})?\|?\*([0-9A-F]{2})\r\n$/,
   ok: /^\$\$([\x41-\x7A])(\d{1,3}),(\d{15}),([0-9A-F]{3}),OK\*([0-9A-F]{2})\r\n$/
-};
+}
 
 const parseAlrm = event => {
   const alarms = {
-    '1': {type: 'SOS_Button'},
-    '2': {type: 'DI', number: 2, status: true},
-    '3': {type: 'DI', number: 3, status: true},
-    '4': {type: 'DI', number: 4, status: true},
-    '5': {type: 'DI', number: 5, status: true},
-    '9': {type: 'DI', number: 1, status: false},
-    '10': {type: 'DI', number: 2, status: false},
-    '11': {type: 'DI', number: 3, status: false},
-    '12': {type: 'DI', number: 4, status: false},
-    '13': {type: 'DI', number: 5, status: false},
-    '17': {type: 'DI', number: 2, status: false},
-    '18': {type: 'lowExternalBattery'},
-    '19': {type: 'Over_Speed', status: true},
-    '20': {type: 'Geo_Fence', status: true},
-    '21': {type: 'Geo_Fence', status: false},
-    '22': {type: 'Charge', status: true},
-    '23': {type: 'Charge', status: false},
-    '24': {type: 'gpsSignal', status: false},
-    '25': {type: 'gpsSignal', status: true},
-    '26': {type: 'Sleep', status: true},
-    '27': {type: 'Sleep', status: false},
-    '28': {type: 'gpsAntennaCut'},
-    '29': {type: 'deviceReboot'},
-    '31': {type: 'Heartbeat'},
-    '32': {type: 'Angle'},
-    '33': {type: 'distanceIntervalTracking'},
-    '34': {type: 'replyCurrent'},
-    '35': {type: 'Gps'},
-    '36': {type: 'tow'},
-    '37': {type: 'Rfid'},
-    '39': {type: 'picture'},
-    '40': {type: 'powerOff'},
-    '41': {type: 'Moving', status: false},
-    '42': {type: 'Moving', status: true},
-    '44': {type: 'jamming', status: true},
-    '50': {type: 'temperature', status: true},
-    '51': {type: 'temperature', status: false},
-    '52': {type: 'fuelFulled'},
-    '53': {type: 'fuelEmpty'},
-    '54': {type: 'fuelStolen'},
-    '56': {type: 'armed'},
-    '57': {type: 'disarmed'},
-    '58': {type: 'stealing'},
-    '63': {type: 'jamming', status: false},
-    '65': {type: 'pressInput1ToCall'},
-    '66': {type: 'pressInput2ToCall'},
-    '67': {type: 'pressInput3ToCall'},
-    '68': {type: 'pressInput4ToCall'},
-    '69': {type: 'pressInput5ToCall'},
-    '70': {type: 'rejectIncomingCall'},
-    '71': {type: 'getLocationByCall'},
-    '72': {type: 'autoAnswerIncomingCall'},
-    '73': {type: 'listenIn'},
-    '79': {type: 'fall'},
-    '80': {type: 'install'},
-    '81': {type: 'dropOff'},
-    '139': {type: 'maintenance'}
-  };
-  return event in alarms ? alarms[event] : null;
-};
+    '1': { type: 'SOS_Button' },
+    '2': { type: 'DI', number: 2, status: true },
+    '3': { type: 'DI', number: 3, status: true },
+    '4': { type: 'DI', number: 4, status: true },
+    '5': { type: 'DI', number: 5, status: true },
+    '9': { type: 'DI', number: 1, status: false },
+    '10': { type: 'DI', number: 2, status: false },
+    '11': { type: 'DI', number: 3, status: false },
+    '12': { type: 'DI', number: 4, status: false },
+    '13': { type: 'DI', number: 5, status: false },
+    '17': { type: 'DI', number: 2, status: false },
+    '18': { type: 'lowExternalBattery' },
+    '19': { type: 'Over_Speed', status: true },
+    '20': { type: 'Geo_Fence', status: true },
+    '21': { type: 'Geo_Fence', status: false },
+    '22': { type: 'Charge', status: true },
+    '23': { type: 'Charge', status: false },
+    '24': { type: 'gpsSignal', status: false },
+    '25': { type: 'gpsSignal', status: true },
+    '26': { type: 'Sleep', status: true },
+    '27': { type: 'Sleep', status: false },
+    '28': { type: 'gpsAntennaCut' },
+    '29': { type: 'deviceReboot' },
+    '31': { type: 'Heartbeat' },
+    '32': { type: 'Angle' },
+    '33': { type: 'distanceIntervalTracking' },
+    '34': { type: 'replyCurrent' },
+    '35': { type: 'Gps' },
+    '36': { type: 'tow' },
+    '37': { type: 'Rfid' },
+    '39': { type: 'picture' },
+    '40': { type: 'powerOff' },
+    '41': { type: 'Moving', status: false },
+    '42': { type: 'Moving', status: true },
+    '44': { type: 'jamming', status: true },
+    '50': { type: 'temperature', status: true },
+    '51': { type: 'temperature', status: false },
+    '52': { type: 'fuelFulled' },
+    '53': { type: 'fuelEmpty' },
+    '54': { type: 'fuelStolen' },
+    '56': { type: 'armed' },
+    '57': { type: 'disarmed' },
+    '58': { type: 'stealing' },
+    '63': { type: 'jamming', status: false },
+    '65': { type: 'pressInput1ToCall' },
+    '66': { type: 'pressInput2ToCall' },
+    '67': { type: 'pressInput3ToCall' },
+    '68': { type: 'pressInput4ToCall' },
+    '69': { type: 'pressInput5ToCall' },
+    '70': { type: 'rejectIncomingCall' },
+    '71': { type: 'getLocationByCall' },
+    '72': { type: 'autoAnswerIncomingCall' },
+    '73': { type: 'listenIn' },
+    '79': { type: 'fall' },
+    '80': { type: 'install' },
+    '81': { type: 'dropOff' },
+    '139': { type: 'maintenance' }
+  }
+  return event in alarms ? alarms[event] : null
+}
 
 const getMvt380 = raw => {
-  const match = patterns.mvt380.exec(raw);
-  const status = match[27].split('').map(x => pad(4, parseInt(x, 10).toString(2), '0')).join('');
+  const match = patterns.mvt380.exec(raw)
+  const status = match[27]
+    .split('')
+    .map(x => pad(4, parseInt(x, 10).toString(2), '0'))
+    .join('')
   const data = {
     raw: match[0],
     type: 'data',
@@ -86,7 +89,12 @@ const getMvt380 = raw => {
       type: 'Point',
       coordinates: [parseFloat(match[7]), parseFloat(match[6])]
     },
-    datetime: new Date(match[8].replace(/(\d{2})(\d{2})(\d{2})(\d{2})(\d{2})(\d{2})/, '20$1-$2-$3T$4:$5:$6.000Z')),
+    datetime: new Date(
+      match[8].replace(
+        /(\d{2})(\d{2})(\d{2})(\d{2})(\d{2})(\d{2})/,
+        '20$1-$2-$3T$4:$5:$6.000Z'
+      )
+    ),
     gpsStatus: match[9] === 'A',
     satellites: parseInt(match[10], 10),
     gsmSignal: parseInt(match[11], 10),
@@ -123,19 +131,19 @@ const getMvt380 = raw => {
       }
     },
     voltage: {
-      ad1: match[28] ? (parseInt(match[28], 16) * 6) / 1024 : null,
-      ad2: match[29] ? (parseInt(match[29], 16) * 6) / 1024 : null,
-      ad3: match[30] ? (parseInt(match[30], 16) * 6) / 1024 : null,
-      battery: match[31] ? (parseInt(match[31], 16) * 3 * 2) / 1024 : null,
-      inputCharge: match[32] ? (parseInt(match[32], 16) * 3 * 16) / 1024 : null
+      ad1: match[28] ? parseInt(match[28], 16) * 6 / 1024 : null,
+      ad2: match[29] ? parseInt(match[29], 16) * 6 / 1024 : null,
+      ad3: match[30] ? parseInt(match[30], 16) * 6 / 1024 : null,
+      battery: match[31] ? parseInt(match[31], 16) * 3 * 2 / 1024 : null,
+      inputCharge: match[32] ? parseInt(match[32], 16) * 3 * 16 / 1024 : null
     }
-  };
-  return data;
-};
+  }
+  return data
+}
 
 const parseCode = raw => {
-  const match = patterns.ok.exec(raw);
-  const code = match[4];
+  const match = patterns.ok.exec(raw)
+  const code = match[4]
   const codes = {
     C01: 'SETIOSWITCH',
     F09: 'CLEARBUF',
@@ -143,95 +151,102 @@ const parseCode = raw => {
     B07: 'SETOVERSPEEDALARM',
     F01: 'RBOOT',
     F02: 'RBOOT'
-  };
+  }
   const data = {
     manufacturer: 'meitrack',
     device: 'MEITRACK-COMMAND-OK',
     type: 'ok',
     code: code
-  };
-  if (Object.keys(codes).indexOf(code) > -1) data.command = codes[code];
-  return data;
-};
+  }
+  if (Object.keys(codes).indexOf(code) > -1) data.command = codes[code]
+  return data
+}
 
 const parse = raw => {
-  let result = {type: 'UNKNOWN', raw: raw.toString()};
+  let result = { type: 'UNKNOWN', raw: raw.toString() }
   if (patterns.mvt380.test(raw)) {
-    result = getMvt380(raw);
+    result = getMvt380(raw)
   } else if (patterns.ok.test(raw)) {
-    result = parseCode(raw);
+    result = parseCode(raw)
   }
-  return result;
-};
+  return result
+}
 
 const isMeitrack = raw => {
-  let result = false;
+  let result = false
   if (patterns.mvt380.test(raw)) {
-    result = true;
+    result = true
   } else if (patterns.ok.test(raw)) {
-    result = true;
+    result = true
   }
-  return result;
-};
+  return result
+}
 
 // Random integer from 65 to 122 (41 to 7a in hex)
 const getRandomDataIdentifier = () => {
-  const int = Math.floor(Math.random() * (122 - 65 + 1) + 65);
-  return String.fromCharCode(int);
-};
+  const int = Math.floor(Math.random() * (122 - 65 + 1) + 65)
+  return String.fromCharCode(int)
+}
 
 const getCommand = (imei, command) => {
-  const raw1 = `,${imei},${command}*`;
-  const raw2 = `@@${getRandomDataIdentifier()}${raw1.length + 4}${raw1}`;
-  return `${raw2}${pad(2, crc.crc1(raw2).toString(16).toUpperCase(), '0')}\r\n`;
-};
+  const raw1 = `,${imei},${command}*`
+  const raw2 = `@@${getRandomDataIdentifier()}${raw1.length + 4}${raw1}`
+  return `${raw2}${pad(
+    2,
+    crc
+      .crc1(raw2)
+      .toString(16)
+      .toUpperCase(),
+    '0'
+  )}\r\n`
+}
 
 const parseCommand = data => {
-  let raw, state, port, speed, interval;
+  let raw, state, port, speed, interval
   if (/^[1-5]{1}_(on|off|status)$/.test(data.instruction)) {
-    let _data = data.instruction.split('_');
-    port = _data[0];
-    state = _data[1];
-    let initial = [2, 2, 2, 2, 2];
-    const states = {off: 0, on: 1, status: 2};
-    initial[port - 1] = states[state];
-    speed = data.speed || 0;
-    raw = `C01,${speed},${initial.join('')}`;
+    let _data = data.instruction.split('_')
+    port = _data[0]
+    state = _data[1]
+    let initial = [2, 2, 2, 2, 2]
+    const states = { off: 0, on: 1, status: 2 }
+    initial[port - 1] = states[state]
+    speed = data.speed || 0
+    raw = `C01,${speed},${initial.join('')}`
   } else if (data.instruction === 'clear_mem') {
-    raw = 'F09,3';
+    raw = 'F09,3'
   } else if (data.instruction === 'set_interval_gprs') {
-    interval = data.interval || 6 * 10;
-    if (interval < 12) interval = 12;
-    let mod = interval % 6;
-    if (mod > 0) interval -= mod;
-    raw = `A12,${interval}`;
+    interval = data.interval || 6 * 10
+    if (interval < 12) interval = 12
+    let mod = interval % 6
+    if (mod > 0) interval -= mod
+    raw = `A12,${interval}`
   } else if (/^set_speed_(on|off)(E)?$/.test(data.instruction)) {
-    speed = data.speed || 0;
-    state = data.instruction.split('_')[2];
-    if (state === 'off') speed = 0;
-    raw = `B07,${speed}`;
-  } else if(data.instruction === 'Custom'){
-    raw = data.command;
+    speed = data.speed || 0
+    state = data.instruction.split('_')[2]
+    if (state === 'off') speed = 0
+    raw = `B07,${speed}`
+  } else if (data.instruction === 'Custom') {
+    raw = data.command
   } else if (/^reboot_gsm$/.test(data.instruction)) {
-    raw = 'F01';
+    raw = 'F01'
   } else if (/^reboot_gps$/.test(data.instruction)) {
-    raw = 'F02';
+    raw = 'F02'
   }
-  return getCommand(data.imei, raw);
-};
+  return getCommand(data.imei, raw)
+}
 
 const getRebootCommand = imei => {
-  return getCommand(imei, 'F02');
-};
+  return getCommand(imei, 'F02')
+}
 
 const getImei = raw => {
-  let imei = null;
-  const data = raw.toString();
+  let imei = null
+  const data = raw.toString()
   if (patterns.mvt380.test(data)) {
-    imei = patterns.mvt380.exec(data)[3];
+    imei = patterns.mvt380.exec(data)[3]
   }
-  return imei;
-};
+  return imei
+}
 
 module.exports = {
   parse: parse,
@@ -241,4 +256,4 @@ module.exports = {
   parseCommand: parseCommand,
   getRebootCommand: getRebootCommand,
   getImei: getImei
-};
+}
--- test/test.js
+++ test/test.js
@@ -1,97 +1,108 @@
-'use strict';
+'use strict'
 
-const meitrack = require('../src');
-const expect = require('chai').expect;
+const meitrack = require('../src')
+const { expect } = require('chai')
+const { describe, it } = require('mocha')
 
 describe('meitrack-parser', () => {
   it('should return the raw data parsed', () => {
-    const raw = new Buffer('$$e155,867965021508656,AAA,35,-33.361133,-70.514245,160412155005,A,8,27,0,289,1.3,867,318379,2338885,730|1|32D3|A03F,0008,0000|0000|0000|02DA|0106,00000001,*3A\r\n');
-    const data = meitrack.parse(raw);
-    expect(data.raw).to.eql(raw.toString());
-    expect(data.manufacturer).to.eql('meitrack');
-    expect(data.device).to.eql('MVT380');
-    expect(data.type).to.eql('data');
-    expect(data.imei).to.eql(867965021508656);
-    expect(data.command).to.eql('AAA');
-    expect(data.alarm.type).to.eql('Gps');
-    expect(data.loc.type).to.eql('Point');
-    expect(data.loc.coordinates).to.eql([-70.514245, -33.361133]);
-    expect(data.datetime).to.eql(new Date('2016-04-12T15:50:05.000Z'));
-    expect(data.gpsStatus).to.be.true;
-    expect(data.satellites).to.eql(8);
-    expect(data.gsmSignal).to.eql(27);
-    expect(data.speed).to.eql(0);
-    expect(data.direction).to.eql(289);
-    expect(data.hdop).to.eql(1.3);
-    expect(data.altitude).to.eql(867);
-    expect(data.odometer).to.eql(318379);
-    expect(data.runtime).to.eql(2338885);
-    expect(data.mcc).to.eql('730');
-    expect(data.mnc).to.eql('1');
-    expect(data.lac).to.eql(13011);
-    expect(data.cid).to.eql(41023);
-    expect(data.status.input['1']).to.be.false;
-    expect(data.status.input['2']).to.be.false;
-    expect(data.status.input['3']).to.be.false;
-    expect(data.status.input['4']).to.be.false;
-    expect(data.status.input['5']).to.be.false;
-    expect(data.status.input['6']).to.be.false;
-    expect(data.status.input['7']).to.be.false;
-    expect(data.status.input['8']).to.be.false;
-    expect(data.status.output['1']).to.be.false;
-    expect(data.status.output['2']).to.be.false;
-    expect(data.status.output['3']).to.be.false;
-    expect(data.status.output['4']).to.be.true;
-    expect(data.status.output['5']).to.be.false;
-    expect(data.status.output['6']).to.be.false;
-    expect(data.status.output['7']).to.be.false;
-    expect(data.status.output['8']).to.be.false;
-  });
+    const raw = Buffer.from(
+      '$$e155,867965021508656,AAA,35,-33.361133,-70.514245,160412155005,A,8,27,0,289,1.3,867,318379,2338885,730|1|32D3|A03F,0008,0000|0000|0000|02DA|0106,00000001,*3A\r\n'
+    )
+    const data = meitrack.parse(raw)
+    expect(data.raw).to.eql(raw.toString())
+    expect(data.manufacturer).to.eql('meitrack')
+    expect(data.device).to.eql('MVT380')
+    expect(data.type).to.eql('data')
+    expect(data.imei).to.eql(867965021508656)
+    expect(data.command).to.eql('AAA')
+    expect(data.alarm.type).to.eql('Gps')
+    expect(data.loc.type).to.eql('Point')
+    expect(data.loc.coordinates).to.eql([-70.514245, -33.361133])
+    expect(data.datetime).to.eql(new Date('2016-04-12T15:50:05.000Z'))
+    expect(data.gpsStatus).to.eql(true)
+    expect(data.satellites).to.eql(8)
+    expect(data.gsmSignal).to.eql(27)
+    expect(data.speed).to.eql(0)
+    expect(data.direction).to.eql(289)
+    expect(data.hdop).to.eql(1.3)
+    expect(data.altitude).to.eql(867)
+    expect(data.odometer).to.eql(318379)
+    expect(data.runtime).to.eql(2338885)
+    expect(data.mcc).to.eql('730')
+    expect(data.mnc).to.eql('1')
+    expect(data.lac).to.eql(13011)
+    expect(data.cid).to.eql(41023)
+    expect(data.status.input['1']).to.eql(false)
+    expect(data.status.input['2']).to.eql(false)
+    expect(data.status.input['3']).to.eql(false)
+    expect(data.status.input['4']).to.eql(false)
+    expect(data.status.input['5']).to.eql(false)
+    expect(data.status.input['6']).to.eql(false)
+    expect(data.status.input['7']).to.eql(false)
+    expect(data.status.input['8']).to.eql(false)
+    expect(data.status.output['1']).to.eql(false)
+    expect(data.status.output['2']).to.eql(false)
+    expect(data.status.output['3']).to.eql(false)
+    expect(data.status.output['4']).to.eql(true)
+    expect(data.status.output['5']).to.eql(false)
+    expect(data.status.output['6']).to.eql(false)
+    expect(data.status.output['7']).to.eql(false)
+    expect(data.status.output['8']).to.eql(false)
+  })
 
   it('should return the true in data', () => {
-    const raw = new Buffer('$$A138,862170013556541,AAA,35,7.092076,79.960473,140412132808,A,10,9,57,275,1,14,5783799,7403612,413|1|F6E0|3933,0000,000B|0009||02D8|0122,*EE\r\n');
-    const data = meitrack.isMeitrack(raw);
-    expect(data).to.be.true;
-  });
+    const raw = Buffer.from(
+      '$$A138,862170013556541,AAA,35,7.092076,79.960473,140412132808,A,10,9,57,275,1,14,5783799,7403612,413|1|F6E0|3933,0000,000B|0009||02D8|0122,*EE\r\n'
+    )
+    const data = meitrack.isMeitrack(raw)
+    expect(data).to.eql(true)
+  })
 
   it('should return the true in response', () => {
-    const raw = new Buffer('$$y28,867965021508656,C01,OK*31\r\n');
-    const data = meitrack.isMeitrack(raw);
-    expect(data).to.be.true;
-  });
+    const raw = Buffer.from('$$y28,867965021508656,C01,OK*31\r\n')
+    const data = meitrack.isMeitrack(raw)
+    expect(data).to.eql(true)
+  })
 
   it('should return ok data', () => {
-    const raw = new Buffer('$$j28,353358017784062,F01,OK*19\r\n');
-    const data = meitrack.parse(raw);
-    expect(data.manufacturer).to.eql('meitrack');
-    expect(data.device).to.eql('MEITRACK-COMMAND-OK');
-    expect(data.type).to.eql('ok');
-    expect(data.code).to.eql('F01');
-  });
+    const raw = Buffer.from('$$j28,353358017784062,F01,OK*19\r\n')
+    const data = meitrack.parse(raw)
+    expect(data.manufacturer).to.eql('meitrack')
+    expect(data.device).to.eql('MEITRACK-COMMAND-OK')
+    expect(data.type).to.eql('ok')
+    expect(data.code).to.eql('F01')
+  })
 
   it('should return command data', () => {
     const command = {
       instruction: '1_on',
       imei: 353358017784062
-    };
-    const data = meitrack.parseCommand(command);
-    expect(data).to.match(/^@@([\x41-\x7A])(\d{1,3}),353358017784062,C01,0,12222\*([0-9A-F]{2})\r\n$/);
-  });
+    }
+    const data = meitrack.parseCommand(command)
+    expect(data).to.match(
+      /^@@([\x41-\x7A])(\d{1,3}),353358017784062,C01,0,12222\*([0-9A-F]{2})\r\n$/
+    )
+  })
 
   it('should return command reboot', () => {
-    const data = meitrack.getRebootCommand(353358017784062);
-    expect(data).to.match(/^@@([\x41-\x7A])(\d{1,3}),353358017784062,F02\*([0-9A-F]{2})\r\n$/);
-  });
+    const data = meitrack.getRebootCommand(353358017784062)
+    expect(data).to.match(
+      /^@@([\x41-\x7A])(\d{1,3}),353358017784062,F02\*([0-9A-F]{2})\r\n$/
+    )
+  })
 
   it('should return null imei', () => {
-    const raw = new Buffer('askdhaskjdhakjdhaksjdhaksjdh');
-    const imei = meitrack.getImei(raw);
-    expect(imei).to.be.null;
-  });
+    const raw = Buffer.from('askdhaskjdhakjdhaksjdhaksjdh')
+    const imei = meitrack.getImei(raw)
+    expect(imei).to.eql(null)
+  })
 
   it('should return MVT380 imei', () => {
-    const raw = new Buffer('$$e155,867965021508656,AAA,35,-33.361133,-70.514245,160412155005,A,8,27,0,289,1.3,867,318379,2338885,730|1|32D3|A03F,0008,0000|0000|0000|02DA|0106,00000001,*3A\r\n');
-    const imei = meitrack.getImei(raw);
-    expect(imei).to.eql('867965021508656');
-  });
-});
+    const raw = Buffer.from(
+      '$$e155,867965021508656,AAA,35,-33.361133,-70.514245,160412155005,A,8,27,0,289,1.3,867,318379,2338885,730|1|32D3|A03F,0008,0000|0000|0000|02DA|0106,00000001,*3A\r\n'
+    )
+    const imei = meitrack.getImei(raw)
+    expect(imei).to.eql('867965021508656')
+  })
+})
